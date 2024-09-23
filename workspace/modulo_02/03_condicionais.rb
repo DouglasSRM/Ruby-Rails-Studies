1b86e9c56018aef14ab5c5ffc78bbfedfce75384
Douglas Seramim <douglasfseramim@gmail.com>
2024-09-23 16:52:59 -0300

    update gitignore

--- workspace/modulo_02/03_condicionais.rb
+++ workspace/modulo_02/03_condicionais.rb
@@ -1,4 +1,19 @@
-x = 3
+# unless / a menos que
+
+print 'Digite um número: '
+x = gets.chomp.to_i
+
+unless x >= 2
+    puts 'x é maior que 2'
+else
+    puts 'x é maior que 2'
+end
+
+# IF
+
+print 'Digite um número: '
+x = gets.chomp.to_i
+
 if x > 2
     puts 'x é maior que 2'
 end

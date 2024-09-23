2612b32d82dd1a36d58cc1947c4fa5bfa63324ca
Douglas Seramim <douglasfseramim@gmail.com>
2024-09-23 17:01:47 -0300

    Update 03_condicionais.rb

--- workspace/modulo_02/03_condicionais.rb
+++ workspace/modulo_02/03_condicionais.rb
@@ -1,3 +1,28 @@
+# Estrutura condicional ternária
+
+sexo = 'F'
+
+sexo == 'M' ? (puts 'Masculino') : (puts 'Feminino')
+
+# casse
+
+print 'Digite uma idade: '
+idade = gets.chomp.to_i
+
+case idade
+when 0..2
+    puts "bebê"
+when 3..12
+    puts "criança"
+when 13..18
+    puts "adolescente"
+when 19..59
+    puts "adulto"
+else
+    puts "idoso"
+end
+
+
 # unless / a menos que
 
 print 'Digite um número: '
@@ -16,4 +41,6 @@ x = gets.chomp.to_i
 
 if x > 2
     puts 'x é maior que 2'
+else
+    puts 'x é menor que 2'
 end

f803af58cb581df88aed8c8560e169b838cc8b6f
Douglas Seramim <douglasfseramim@gmail.com>
2024-09-25 20:52:48 -0300

    update

--- curso-ruby-poo/jogos/adivinhar_numero/lib/inicializacao.rb
+++ curso-ruby-poo/jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,14 +1,27 @@
+require 'tty-cursor'
+require 'tty-spinner'
+
 class Inicializacao
 	def self.inicializando
 		system('cls')
 
-		print 'Inicializando'
-		2.times do |i|	
-			print '.'
-			sleep 1
-		end
-		puts '.'
-		sleep 1
+		cursor = TTY::Cursor
+		print cursor.move_to(30, 20)
+
+		spinner = TTY::Spinner.new
+		spinner = TTY::Spinner.new("[:spinner] Carregando ...", format: :pulse_2)
+		spinner.auto_spin
+		sleep(2)
+		spinner.stop('Pronto!')
+		
+
+	#	print 'Inicializando'
+	#	2.times do |i|	
+	#		print '.'
+	#		sleep 1
+	#	end
+	#	puts '.'
+	#	sleep 1
 
 		system('cls')
 	end

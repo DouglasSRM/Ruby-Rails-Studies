60d3be50c594dffc7f50b8160659926bd1526fd5
Douglas Seramim <douglasfseramim@gmail.com>
2024-09-26 19:39:45 -0300

    update

--- curso-ruby-poo/jogos/adivinhar_numero/lib/sortear_numero.rb
+++ curso-ruby-poo/jogos/adivinhar_numero/lib/sortear_numero.rb
@@ -1,10 +1,11 @@
 class SortearNumero
 	def self.sortear
+		
+		#return rand(1..100)
 		v = []
-
 		File.open(File.expand_path('../../numeros.txt', __FILE__), 'r') do |arq|
 			while line = arq.gets
-			v.push(line.to_i)
+				v.push(line.to_i)
 			end
 		end
 
